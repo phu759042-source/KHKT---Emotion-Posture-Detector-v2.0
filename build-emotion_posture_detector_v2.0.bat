287296bbd45257c70693d876eaf38a5452ee6b8f
phu759042-source <phu759042@gmail.com>
2025-10-01 17:02:30 +0700

    Update and rename build-emotion_posture_detector.bat to build-emotion_posture_detector_v2.0.bat

--- build-emotion_posture_detector_v2.0.bat
+++ build-emotion_posture_detector_v2.0.bat
@@ -6,4 +6,5 @@ pyinstaller --onefile ^
   --add-data "emotion_posture_detector.ico;." ^
   --add-data "ARIALBD 1.ttf;." ^
   --add-data "C:\Users\ADMIN\AppData\Local\Programs\Python\Python310\Lib\site-packages\mediapipe\modules;mediapipe/modules" ^
-  emotion_posture_detector.py
+  emotion_posture_detector_v2.0.py
+
